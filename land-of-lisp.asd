ddc2a026c7d2eff926f6ea2278c0819b3213081b
Gaelan D'costa <gdcosta@gmail.com>
2013-05-21 21:18:40 -0400

    Initial commit (up to chapter 7)

---
+++ land-of-lisp.asd
@@ -0,0 +1,10 @@
+;;;; land-of-lisp.asd
+
+(asdf:defsystem #:land-of-lisp
+  :serial t
+  :description "Describe land-of-lisp here"
+  :author "Gaelan D'costa"
+  :license "Public Domain"
+  :components ((:file "package")
+               (:file "land-of-lisp")))
+
---
+++ land-of-lisp.lisp
@@ -0,0 +1,221 @@
+;;;; land-of-lisp.lisp
+
+(in-package #:land-of-lisp)
+
+;;; Guessing game goes over here.
+(defparameter *guess-high* 100
+  "Starting upper limit for the guessing game.")
+(defparameter *guess-low* 0
+  "Starting lower bound for the guessing game.")
+
+(defun guess-number ()
+  (ash (+ *guess-high* *guess-low*) -1))
+
+(defun bigger ()
+  (setf *guess-low* (1+ (guess-number)))
+  (guess-number))
+
+(defun smaller ()
+  (setf *guess-high* (1- (guess-number)))
+  (guess-number))
+
+(defun start-over ()
+  (defparameter *guess-high* 100)
+  (defparameter *guess-low* 0)
+  (guess-number))
+
+;;; print-eval fun
+(defun say-hello ()
+  (princ "Please type your name:")
+  (let ((name (read-line)))
+    (princ "Nice to meet you, ")
+    (princ name)))
+
+(defun add-five ()
+  (print "please enter a number:")
+  (let ((num (read)))
+    (print "When I add five I get")
+    (print (+ num 5))))
+
+
+
+;;; Text adventur
+(defparameter *nodes* '((living-room (You are in the living room. A wizard is snoring loudly on the couch.))
+			(garden (You are in a beautiful garden. There is a well in front of you))
+			(attic (You are in the attic. There is a giant welding torch in the corner.))))
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(defparameter *edges* '((living-room (garden west door)
+			 (attic upstairs ladder))
+			(garden (living-room east door))
+			(attic (living-room downstairs ladder))))
+
+(defun describe-path (edge)
+  `(there is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+(defparameter *objects* '(whiskey bucket frog chain))
+
+(defparameter *object-locations* '((whiskey living-room)
+				   (bucket living-room)
+				   (chain garden)
+				   (frog garden)))
+
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+	     (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-loc)
+  (labels ((describe-obj (obj)
+	     `(You see a ,obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-loc)))))
+
+(defparameter *location* 'living-room)
+
+(defun look ()
+  (append (describe-location *location* *nodes*)
+	  (describe-paths *location* *edges*)
+	  (describe-objects *location* *objects* *object-locations*)))
+
+(defun walk (direction)
+  (let ((next (find direction (cdr (assoc *location* *edges*))
+		    :key #'cadr)))
+    (if next
+	(progn (setf *location* (car next))
+	       (look))
+	'(you cannot go that way.))))
+
+(defun pickup (object)
+  (cond ((member object
+		 (objects-at *location* *objects* *object-locations*))
+	 (push (list object 'body) *object-locations*)
+	 `(you are now carrying the ,object))
+	(t '(you cannot get that.))))
+
+(defun inventory ()
+  (cons 'items- (objects-at 'body *objects* *object-locations*)))
+
+(defun game-repl ()
+  (let ((cmd (game-read)))
+    (unless (eq (car cmd) 'quit)
+      (game-print (game-eval cmd))
+      (game-repl))))
+
+(defun game-read ()
+  (let ((cmd (read-from-string
+	      (concatenate 'string "(" (read-line) ")"))))
+    (flet ((quote-it (x)
+	     (list 'quote x)))
+      (cons (car cmd) (mapcar #'quote-it (cdr cmd))))))
+
+(defparameter *allowed-commands* '(look walk pickup inventory))
+
+(defun game-eval (sexp)
+  (if (member (car sexp) *allowed-commands*)
+      (eval sexp)
+      '(I do not know that command.)))
+
+(defun tweak-text (lst caps lit)
+  (when lst
+    (let ((item (car lst))
+	  (rest (cdr lst)))
+      (cond ((eql item #\space) (cons item (tweak-text rest caps lit)))
+	    ((member item '(#\! #\? #\.)) (cons item (tweak-text rest t lit)))
+	    ((eql item #\") (tweak-text rest caps (not lit)))
+	    (lit (cons item (tweak-text rest nil lit)))
+	    (caps (cons (char-upcase item) (tweak-text rest nil lit)))
+	    (t (cons (char-downcase item) (tweak-text rest nil nil)))))))
+
+(defun game-print (lst)
+  (princ (coerce (tweak-text (coerce (string-trim "() "
+						  (prin1-to-string lst))
+				     'list)
+			     t nil)
+		 'string))
+  (fresh-line))
+
+(defun dot-name (exp)
+  (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string exp)))
+
+(defparameter *max-label-length* 30)
+
+(defun dot-label (exp)
+  (if exp
+      (let ((s (write-to-string exp :pretty nil)))
+	(if (> (length s) *max-label-length*)
+	    (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+	    s))
+      ""))
+
+(defun nodes->dot (nodes)
+  (mapc (lambda (node)
+	  (fresh-line)
+	  (princ (dot-name (car node)))
+	  (princ "[label=\"")
+	  (princ (dot-label node))
+	  (princ "\"];"))
+	nodes))
+
+(defun edges->dot (edges)
+  (mapc (lambda (node)
+	  (mapc (lambda (edge)
+		  (fresh-line)
+		  (princ (dot-name (car node)))
+		  (princ "->")
+		  (princ (dot-name (car edge)))
+		  (princ "[label=\"")
+		  (princ (dot-label (cdr edge)))
+		  (princ "\"];"))
+		(cdr node)))
+	edges))
+
+(defun graph->dot (nodes edges)
+  (princ "digraph{")
+  (nodes->dot nodes)
+  (edges->dot edges)
+  (princ "}"))
+
+(defun dot->png (fname thunk)
+  (with-open-file (*standard-output*
+		   fname
+		   :direction :output
+		   :if-exists :supersede)
+    (funcall thunk))
+  (ccl:run-program "bash" '("-c" "/usr/local/bin/dot -Tpng")
+	       :input fname
+	       :output (concatenate 'string fname ".png")))
+
+(defun graph->png (fname nodes edges)
+  (dot->png fname
+	    (lambda ()
+	      (graph->dot nodes edges))))
+
+(defun uedges->dot (edges)
+  (maplist (lambda (lst)
+	     (mapc (lambda (edge)
+		     (unless (assoc (car edge) (cdr lst))
+		       (fresh-line)
+		       (princ (dot-name (caar lst)))
+		       (princ "--")
+		       (princ (dot-name (car edge)))
+		       (princ "[lambda=\"")
+		       (princ (dot-label (cdr edge)))
+		       (princ "\"];")))
+		   (cdar lst)))
+	   edges))
+
+(defun ugraph->dot (nodes edges)
+  (princ "graph{")
+  (nodes->dot nodes)
+  (uedges->dot edges)
+  (princ "}"))
+
+(defun ugraph->png (fname nodes edges)
+  (dot->png fname
+	    (lambda ()
+	      (ugraph->dot nodes edges))))
